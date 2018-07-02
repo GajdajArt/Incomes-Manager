d5a72c59193f96980161e493b0d4a7974fd13a77
gajdaj <artem.gajdaj@nixsolutions.com>
2018-07-02 11:19:51 +0300

    dagger 2/11 update

--- app/src/main/java/com/example/gajdaj/myapplication/ui/BaseFragment.java
+++ app/src/main/java/com/example/gajdaj/myapplication/ui/BaseFragment.java
@@ -1,12 +1,5 @@
 package com.example.gajdaj.myapplication.ui;
 
-import android.os.Bundle;
-import android.support.annotation.Nullable;
-import android.support.v4.app.Fragment;
-
-import com.example.gajdaj.myapplication.presentation.PresenterView;
-
-import dagger.android.support.AndroidSupportInjection;
 import dagger.android.support.DaggerFragment;
 
 public abstract class BaseFragment extends DaggerFragment{
